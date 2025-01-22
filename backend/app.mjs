5cadd2fbf7bf142097f40f9c63a3b210672320ee
Prakash Pathak <prakash@MacBook-Air.local>
2025-01-22 18:14:46 +0530

    first commit

---
+++ backend/app.mjs
@@ -0,0 +1,40 @@
+import AWS from 'aws-sdk';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.PADOSI_TABLE_NAME || 'HomeKitchens';
+
+export const lambdaHandler = async (event, context) => {
+    // Retrieve the item from the DynamoDB table
+    const getParams = {
+        TableName: tableName,
+        Key: {
+            id: event.pathParameters.id
+        }
+    };
+
+    try {
+        const data = await dynamoDb.get(getParams).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Item)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Could not retrieve item' })
+        };
+    }
+};
